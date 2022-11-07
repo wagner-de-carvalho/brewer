6ba34e9e5f88ba37ebfc56c4bce69b66c2d20050
wagner <wagncarv@outlook.com>
2022-11-07 15:58:38 -0300

    3.6. Começando o cadastro de cerveja

---
+++ src/main/java/com/home/brewer/model/Cerveja.java
@@ -0,0 +1,42 @@
+package com.home.brewer.model;
+
+import java.util.Objects;
+
+public class Cerveja {
+	private String sku;
+	private String nome;
+
+	public String getSku() {
+		return sku;
+	}
+
+	public void setSku(String sku) {
+		this.sku = sku;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(sku);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cerveja other = (Cerveja) obj;
+		return Objects.equals(sku, other.sku);
+	}
+
+}
