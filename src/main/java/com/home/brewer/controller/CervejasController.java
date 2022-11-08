f8e29d44dc763ccadc8c9aecd00f2582556aed76
wagner <wagncarv@outlook.com>
2022-11-08 12:41:49 -0300

    3.8. Forward e Redirect

--- src/main/java/com/home/brewer/controller/CervejasController.java
+++ src/main/java/com/home/brewer/controller/CervejasController.java
@@ -3,9 +3,11 @@ package com.home.brewer.controller;
 import javax.validation.Valid;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.home.brewer.model.Cerveja;
 
@@ -18,11 +20,14 @@ public class CervejasController {
 	}
 
 	@RequestMapping(value = "/cervejas/novo", method = RequestMethod.POST)
-	public String cadastrar(@Valid Cerveja cerveja, BindingResult result) {
+	public String cadastrar(@Valid Cerveja cerveja, BindingResult result, Model model, RedirectAttributes attr) {
 		if (result.hasErrors()) {
-			System.out.println("Há erros!");
+			model.addAttribute("mensagem", "Erro no formulario!\n");
+			return "cerveja/CadastroCerveja";
 		}
+		
+		attr.addFlashAttribute("mensagem", "Cerveja gravada com sucesso!");
 		System.out.println("Cadastro Cerveja >>> " + cerveja.getSku() + ", " + cerveja.getNome());
-		return "cerveja/CadastroCerveja";
+		return "redirect:/cervejas/novo";
 	}
 }
