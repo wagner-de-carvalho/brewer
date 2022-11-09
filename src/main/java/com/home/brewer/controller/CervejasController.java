83ff273dc83ee44908bb1029e7921426f1e89d6d
wagner <wagncarv@outlook.com>
2022-11-09 11:03:02 -0300

    5.14. Desafio: prototipando o cadastro de estilo

--- src/main/java/com/home/brewer/controller/CervejasController.java
+++ src/main/java/com/home/brewer/controller/CervejasController.java
@@ -31,7 +31,7 @@ public class CervejasController {
 	
 	@RequestMapping("/cervejas/cadastro")
 	public String cadastro() {
-		return "cidade/CadastroCidade";
+		return "estilo/CadastroEstilo";
 	}
 	
 }
