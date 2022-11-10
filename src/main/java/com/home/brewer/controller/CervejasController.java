ad8a85e00963312302059060189a183bb0322239
wagner <wagncarv@outlook.com>
2022-11-10 00:12:36 -0300

    9.2. Configurando o projeto

--- src/main/java/com/home/brewer/controller/CervejasController.java
+++ src/main/java/com/home/brewer/controller/CervejasController.java
@@ -4,6 +4,7 @@ import javax.validation.Valid;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -12,16 +13,21 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.home.brewer.model.Cerveja;
+import com.home.brewer.repository.Cervejas;
 
 @Controller
 public class CervejasController {
 	private static final Logger logger = LoggerFactory.getLogger(CervejasController.class);
+
+//	@Autowired
+//	private Cervejas cervejas;
 
 	@RequestMapping("/cervejas/novo")
 	public String novo(Cerveja cerveja) {
 		if (logger.isDebugEnabled()) {
 			logger.debug("Objeto cerveja: " + cerveja);
 		}
+//		cervejas.findAll(); //TODO remover
 		return "cerveja/CadastroCerveja";
 	}
 
